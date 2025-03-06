17abb2897f39cbffe10669c7b5c67d71da8db39f
yun7557 <yugi7557@gmail.com>
2025-03-06 15:41:56 +0900

    change

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,8 +9,7 @@ import { BldgModule } from './bldg/bldg.module';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.CLOUDSQL_HOST || `/cloudsql/${process.env.CLOUDSQL_INSTANCE_CONNECTION_NAME}`,
-      // host: process.env.CLOUDSQL_HOST, // 로컬에서 실행시
-      port: parseInt('5432'),
+      port: 5432,
       username: process.env.CLOUDSQL_USER,
       password: process.env.CLOUDSQL_PASS,
       database: process.env.CLOUDSQL_DB,
