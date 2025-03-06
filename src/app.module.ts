08d6337369a6396e02feaf3021382d057c3a1b59
yun7557 <yugi7557@gmail.com>
2025-03-06 15:33:10 +0900

    port error solve??

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,8 +8,8 @@ import { BldgModule } from './bldg/bldg.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.CLOUDSQL_HOST,
-      port: 5432, 
+      host: process.env.CLOUDSQL_HOST || `/cloudsql/${process.env.CLOUDSQL_INSTANCE_CONNECTION_NAME}`,
+      port: parseInt('5432'),
       username: process.env.CLOUDSQL_USER,
       password: process.env.CLOUDSQL_PASS,
       database: process.env.CLOUDSQL_DB,
